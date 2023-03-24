1a0612bbbc8122982e2424bf1226666a7e00a044
TheWildWestBoy03 <alexandru.pogan@stud.acs.upb.ro>
2023-03-24 21:57:18 +0200

    Change in main hh

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include "listOfTeams.h"
 
 int main(){
-    int numberOfLists;
+    int numberOfTeams;
     Team *listOfTeamsHead = NULL;
     FILE *teamsFiles;
     for(int i=0; i < numberOfTeams; i++){
         Data numberOfMembers;
-        char *nameOfTeam = 
+        char *nameOfTeam;
         int x;
         printf("The number of members in this team is: ");
         scanf("%d", &x);
