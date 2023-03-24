81d1719f3380975d9277884777112284ab72e85b
TheWildWestBoy03 <alexandru.pogan@stud.acs.upb.ro>
2023-03-24 21:42:12 +0200

    I added some undefined functionality on lists.

---
+++ listOfTeams.c
@@ -0,0 +1,22 @@
+/*
+    In this file, i defined the body of the most important functions for 
+    manipulating the lists.
+*/
+#include "listOfTeams.h"
+
+void addingAtBeginning(Team **listOfTeamHead, Data numberOfMembers, char *nameOfTeam){
+    Team *current = (Team*)malloc(sizeof(Team));
+    char bufferNameTeam[50];
+    current -> next = NULL;
+    current -> numberOfMembers = numberOfMembers;
+    current -> nameOfTeam = (char*) malloc(sizeof(char)*strlen(nameOfTeam));
+    if(*ListOfTeamHead == NULL){
+        *listOfTeamHead = current;
+        return;
+    }
+    else{
+        current -> next = *listOfTeamHead;
+        *listOfTeamHead = current;
+        return;
+    }
+}
