53a189745f95d9aff6653fdf8563314dfe5e8e51
TheWildWestBoy03 <alexandru.pogan@stud.acs.upb.ro>
2023-03-24 21:43:11 +0200

    Nevermind

---
+++ listOfTeams.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef int Data;
+typedef struct Team{
+    Data numberOfMembers;
+    char *nameOfTeam;
+    char **nameOfColleagues;
+    struct Team *next;  
+}Team;
+
+void addingAtBeginning(Team *listOfTeamHead, Data numberOfMembers, char *nameOfTeam);
+
---
+++ main.c
@@ -0,0 +1,16 @@
+#include "listOfTeams.h"
+
+int main(){
+    int numberOfLists;
+    Team *listOfTeamsHead = NULL;
+    FILE *teamsFiles;
+    for(int i=0; i < numberOfTeams; i++){
+        Data numberOfMembers;
+        char *nameOfTeam = 
+        int x;
+        printf("The number of members in this team is: ");
+        scanf("%d", &x);
+        numberOfMembers = x;
+        addingAtBeginning(&listOfTeamsHead, numberOfMembers, nameOfTeam);
+    }
+}
