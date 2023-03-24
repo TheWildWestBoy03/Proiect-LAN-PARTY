3e5119d49399fc6a4282fb2d1e5a6570e0dd09be
TheWildWestBoy03 <alexandru.pogan@stud.acs.upb.ro>
2023-03-24 23:09:58 +0200

    Modified main

--- main.c
+++ main.c
@@ -4,13 +4,5 @@ int main(){
     int numberOfTeams;
     Team *listOfTeamsHead = NULL;
     FILE *teamsFiles;
-    for(int i=0; i < numberOfTeams; i++){
-        Data numberOfMembers;
-        char *nameOfTeam;
-        int x;
-        printf("The number of members in this team is: ");
-        scanf("%d", &x);
-        numberOfMembers = x;
-        addingAtBeginning(&listOfTeamsHead, numberOfMembers, nameOfTeam);
-    }
+    void readingData();
 }
