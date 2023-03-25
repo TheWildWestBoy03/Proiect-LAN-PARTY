2a9071b4653604a2f0fd1d9d97406837044ef7eb
TheWildWestBoy03 <alexandru.pogan@stud.acs.upb.ro>
2023-03-25 19:23:26 +0200

    Added an include to a new file

--- listOfTeams.h
+++ listOfTeams.h
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "Player.h"
 typedef int Data;
+
 typedef struct Team{
     Data numberOfMembers;
     char *nameOfTeam;
     struct Team *next; 
-    struct Player *playersInTeam;
-}Team
+    Player *playerInTeams;
+}Team;
 
-typedef struct Player{
-    char *firstName;
-    char *secondName;
-    int points;
-}Player;
 void addingAtBeginning(Team **listOfTeamHead, Data numberOfMembers, char *nameOfTeam, Team *current);
 void addTheTeams(Team **listOfTeamHead, int numberOfTeams);
 void displayTheList(Team *listOfTeamHead);
