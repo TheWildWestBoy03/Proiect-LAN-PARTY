aa7f34c9e0c9b66df88adc65aae7957ca394f4d1
TheWildWestBoy03 <alexandru.pogan@stud.acs.upb.ro>
2023-03-25 18:36:31 +0200

    Some change in the template of list

--- listOfTeams.h
+++ listOfTeams.h
@@ -6,9 +6,14 @@ typedef struct Team{
     Data numberOfMembers;
     char *nameOfTeam;
     char **nameOfColleagues;
-    struct Team *next;  
-}Team;
-
+    struct Team *next; 
+    struct Player *player;
+}Team
+typedef struct Player{
+    char *firstName;
+    char *secondName;
+    int punctaj;
+}Player;
 void addingAtBeginning(Team **listOfTeamHead, Data numberOfMembers, char *nameOfTeam, Team *current);
 void addTheTeams(Team **listOfTeamHead, int numberOfTeams);
 void displayTheList(Team *listOfTeamHead);
