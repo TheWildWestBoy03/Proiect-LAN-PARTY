236e32b60f51071623d6a7ed0fb602205cf87eaa
TheWildWestBoy03 <alexandru.pogan@stud.acs.upb.ro>
2023-05-20 19:31:57 +0300

    These files were added to simplify the main.c file

--- cerinte.h
+++ cerinte.h
@@ -1,2 +1,14 @@
 #include "matchesQueue.h"
 
+void solveTheFirstTask(Team *listOfTeamsHead, FILE **outputFile);
+void solveTheSecondTask(Team *listOfTeamsHead, FILE **outputFile, int numberOfTeams);
+void solveTheThirdTaskPt1(Team **listOfTeamsHead, FILE **outputFile, int *numberOfTeams);
+void printQueueLine(FILE **outputFile, QMatch *currentMatch);
+void printStackLine(FILE **outputFile, Stack *currentWinner, Stack *currentWinnerOpponent);
+WinnersList *addTheWInnersUtil(WinnersList *gameWinners, Stack *currentWinner, Stack *currentWinnerOpponent);
+QMatch *prepairTheScheduledMatch(Stack *currentWinner, Stack *currentWinnerOpponent);
+Stack *getTheFinalWinner(QMatch *finalMatch);
+void simulateFinalMatch(QueueOfMatches *queueOfMatches, Stack *currentWinner, Stack *currentWinnerOpponent, FILE **outputFile, int numberOfRounds);
+void simulateTheGame(QueueOfMatches *queueOfMatches, FILE **outputFile, Team *listOfTeamsHead, int numberOfTeams, WinnersList **gameOfficialWinners);
+void solveTheFourthTask(WinnersList *gameWinners, FILE **outputFile, WinnersTree **binarySearchTreeRoot);
+void solveTheLastTask(WinnersTree *binarySearchTreeRoot, AVLNode **avlRoot, FILE **outputFile);
